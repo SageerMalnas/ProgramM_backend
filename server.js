212988b87b5399d4157941e01cf51b77c6932e9b
SageerMalnas <sageermalnas78@gmail.com>
2024-10-17 23:10:45 +0530

    added register user

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express")
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
 const connectDB = require("./config/db");
+const authRoutes = require('./routes/authRoutes');
+
 
 dotenv.config();
-connectDB();
 
 const app = express();
-
 app.use(express.json());
 
-const PORT = 5000;
+connectDB();
+app.use('/api/auth',authRoutes);
+app.use((err,req,res,next) =>{
+    res.status(500).json({message: err.message});
+})
+
+app.get('/', (req,res) =>{
+    res.send('Hello server');
+})
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
