ecea05a4c57395e3ad17b4e1e87f0ce7ce017a83
SageerMalnas <sageermalnas78@gmail.com>
2024-10-18 12:08:18 +0530

    handled cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express")
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
-
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 app.use('/api/users',userRoutes);
