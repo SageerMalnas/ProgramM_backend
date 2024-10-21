d85ba0a18f1531483b033623a1b565b45a291472
SageerMalnas <sageermalnas78@gmail.com>
2024-10-21 13:24:03 +0530

    added edit, analytics and delete task api

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require('../schema/taskModel');
 const User = require('../schema/userModel');
 
+
 // Get task with filter
 exports.getTasks = async (req, res) => {
   try {
@@ -28,15 +29,15 @@ exports.createTask = async (req, res) => {
   try {
     const { title, priority, checklists, dueDate, status, assignedTo } = req.body;
     if (!title || !priority || !checklists || checklists.length === 0) {
-        return res.status(400).json({ message: "Title, priority, and at least one checklist are required." });
-      }
-  
-      const usersExist = await User.find({ _id: { $in: assignedTo } });
+      return res.status(400).json({ message: "Title, priority, and at least one checklist are required." });
+    }
+
+    const usersExist = await User.find({ _id: { $in: assignedTo } });
     //   if (usersExist?.length !== assignedTo?.length) {
     //     return res.status(400).json({ message: "Some assigned users do not exist." });
     //   }
 
-      const validChecklists = Array.isArray(checklists) ? checklists : [];
+    const validChecklists = Array.isArray(checklists) ? checklists : [];
 
     const newTask = new Task({
       title,
@@ -56,3 +57,87 @@ exports.createTask = async (req, res) => {
   }
 };
 
+// Get a specific task
+exports.getTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.taskId).populate('assignedTo');
+    if (!task) {
+      return res.status(404).json({ success: false, message: "Task not found" });
+    }
+    res.status(200).json({ success: true, task });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to fetch the task" });
+  }
+};
+
+//update task
+exports.editTask = async (req, res) => {
+  try {
+    const { title, priority, checklists, dueDate, status, assignedTo } = req.body;
+
+    const updatedTask = await Task.findOneAndUpdate(
+      { _id: req.params.taskId, createdBy: req.user._id },
+      { title, priority, checklists, dueDate, status, assignedTo },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ success: false, message: "Task not found" });
+    }
+
+    res.status(200).json({ success: true, message: "Task updated successfully", task: updatedTask });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Task update failed" });
+  }
+};
+
+// Delete a task
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.taskId,
+      createdBy: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).json({ success: false, message: "Task not found" });
+    }
+
+    res.status(204).json({ success: true, message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to delete task" });
+  }
+};
+
+// Task analytics
+exports.analytics = async (req, res) => {
+  try {
+    const tasks = await Task.find({ createdBy: req.user._id });
+
+    const status = {
+      backlog: 0,
+      todo: 0,
+      inProgress: 0,
+      done: 0,
+    };
+
+    const priorities = {
+      low: 0,
+      moderate: 0,
+      high: 0,
+      due: 0,
+    };
+
+    tasks.forEach(task => {
+      status[task.status]++;
+      priorities[task.priority]++;
+      if (new Date() > task.dueDate && task.status !== 'done') {
+        priorities.due++;
+      }
+    });
+
+    res.status(200).json({ success: true, data: { status, priorities } });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to get analytics" });
+  }
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const { createTask,getTasks,getTask,updateTask,deleteTask,analytics} = require('../controllers/taskController');
+const { createTask,getTasks,getTask,editTask,deleteTask,analytics} = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', protect, getTasks);
 router.post('/', protect, createTask);
+router.get('/analytics', protect, analytics);
+router.get('/:taskId', protect, getTask);
+router.put('/:taskId', protect, editTask);
+router.delete('/:taskId', protect, deleteTask);
 
 
 module.exports = router;
